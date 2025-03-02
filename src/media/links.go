c7028eb8034f711c36a02ecb016c2b9317664f87
devlikepro <waha@devlike.pro>
2025-03-02 15:13:44 +0700

    Add go test

--- src/media/links.go
+++ src/media/links.go
@@ -7,7 +7,7 @@ import (
 	"strings"
 )
 
-var UrlRegex = `/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/`
+var UrlRegex = `(http(s)?:\/\/.)(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)`
 var UrlRe = regexp.MustCompile(UrlRegex)
 
 func ExtractUrlFromText(text string) string {
