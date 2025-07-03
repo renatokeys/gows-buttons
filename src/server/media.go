36f77866125452e12187fe3f6c88f20edb5660df
devlikepro <waha@devlike.pro>
2025-07-03 16:31:30 +0700

    fallback to message storage in case of errors

--- src/server/media.go
+++ src/server/media.go
@@ -12,13 +12,33 @@ func (s *Server) DownloadMedia(ctx context.Context, req *__.DownloadMediaRequest
 	if err != nil {
 		return nil, err
 	}
-	msg, err := BuildMessage(req.GetMessage())
-	if err != nil {
-		return nil, err
+	// Parse Message from JSON provided
+	msg, buildMessageError := BuildMessage(req.GetMessage())
+	if buildMessageError != nil {
+		cli.Log.Warnf("Failed to build message from JSON: %v", buildMessageError)
 	}
+
+	// If parsing JSON failed - fetch it from storage
+	if msg == nil && req.MessageId != "" {
+		cli.Log.Debugf("Fetching message from storage '%s'", req.MessageId)
+		storedMessage, err := cli.Storage.Messages.GetMessage(req.GetMessageId())
+		if err != nil {
+			cli.Log.Warnf("Failed to fetch message '%s' from storage: %v", req.MessageId, err)
+		}
+		if storedMessage != nil {
+			cli.Log.Infof("Found message '%s' in storage, using it to fetch media", req.MessageId)
+			msg = storedMessage.Message.Message
+		}
+	}
+
+	if msg == nil {
+		cli.Log.Warnf("Failed to build message '%s' from JSON or fetch storage", req.MessageId)
+		return nil, buildMessageError
+	}
+
 	resp, err := cli.DownloadAny(ctx, msg)
 	if err != nil {
-		cli.Log.Errorf("Failed to download media: %v", err)
+		cli.Log.Errorf("Failed to download media for '%s' message: %v", req.MessageId, err)
 		return nil, nil
 	}
 	return &__.DownloadMediaResponse{Content: resp}, nil
