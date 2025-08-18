f06828bf470b09f23bcbc54acd9604c391876b20
devlikepro <waha@devlike.pro>
2025-08-18 12:09:24 +0700

    Send status only to @s.whatsapp.net

--- src/gows/status.go
+++ src/gows/status.go
@@ -29,19 +29,32 @@ func (gows *GoWS) SendStatusMessage(ctx context.Context, to types.JID, msg *waE2
 		mutable.Reverse(allParticipants)
 	}
 
+	// Filter out only the right participants
+	validParticipants := lo.Filter(allParticipants, func(p types.JID, _ int) bool {
+		return p.Server == types.DefaultUserServer
+	})
+
 	participantsBatchSize := StatusParticipantsBatchSize
 	if len(extra.Participants) > 0 {
 		// If participants are provided, use the batch size of the participants
 		participantsBatchSize = len(extra.Participants)
 	}
 
-	batches := lo.Chunk(allParticipants, participantsBatchSize)
+	batches := lo.Chunk(validParticipants, participantsBatchSize)
 	if extra.ID == "" {
 		extra.ID = gows.Client.GenerateMessageID()
 	}
 
 	errs := make([]error, 0)
-	gows.Log.Infof("Sending status message (%s) in %d batches - %d participants in total, %d per batch", extra.ID, len(batches), len(allParticipants), participantsBatchSize)
+	ignored := len(allParticipants) - len(validParticipants)
+	gows.Log.Infof(
+		"Sending status message (%s) in %d batches - %d participants in total, %d per batch, %d ignored",
+		extra.ID,
+		len(batches),
+		len(validParticipants),
+		participantsBatchSize,
+		ignored,
+	)
 	for index, participants := range batches {
 		batchExtra := extra
 		batchExtra.Participants = participants
