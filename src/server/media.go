122dbef18f2051a6426cece87d14ee8ae78114e6
devlikepro <waha@devlike.pro>
2025-11-28 11:57:47 +0700

    10 min timeout for media

--- src/server/media.go
+++ src/server/media.go
@@ -5,9 +5,17 @@ import (
 	"encoding/json"
 	"github.com/devlikeapro/gows/proto"
 	"go.mau.fi/whatsmeow/proto/waE2E"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"time"
 )
 
+const downloadMediaTimeout = 10 * time.Minute
+
 func (s *Server) DownloadMedia(ctx context.Context, req *__.DownloadMediaRequest) (*__.DownloadMediaResponse, error) {
+	ctx, cancel := context.WithTimeout(ctx, downloadMediaTimeout)
+	defer cancel()
+
 	cli, err := s.Sm.Get(req.GetSession().GetId())
 	if err != nil {
 		return nil, err
@@ -27,19 +35,36 @@ func (s *Server) DownloadMedia(ctx context.Context, req *__.DownloadMediaRequest
 		}
 		if storedMessage != nil {
 			cli.Log.Infof("Found message '%s' in storage, using it to fetch media", req.MessageId)
-			msg = storedMessage.Message.Message
+			switch {
+			case storedMessage.Message != nil && storedMessage.Message.Message != nil:
+				msg = storedMessage.Message.Message
+			case storedMessage.Message != nil && storedMessage.Message.RawMessage != nil:
+				// History sync messages may only have RawMessage populated.
+				msg = storedMessage.Message.RawMessage
+			}
 		}
 	}
 
 	if msg == nil {
 		cli.Log.Warnf("Failed to build message '%s' from JSON or fetch storage", req.MessageId)
-		return nil, buildMessageError
+		if buildMessageError != nil {
+			return nil, status.Errorf(codes.InvalidArgument, "failed to parse message: %v", buildMessageError)
+		}
+		return nil, status.Error(codes.InvalidArgument, "message is empty")
+	}
+
+	if ctx.Err() != nil {
+		return nil, status.Error(codes.DeadlineExceeded, "download media timed out before start")
 	}
 
 	resp, err := cli.DownloadAny(ctx, msg)
 	if err != nil {
+		if ctx.Err() != nil {
+			cli.Log.Warnf("Media download for '%s' canceled: %v", req.MessageId, ctx.Err())
+			return nil, status.Error(codes.DeadlineExceeded, "download media timed out")
+		}
 		cli.Log.Errorf("Failed to download media for '%s' message: %v", req.MessageId, err)
-		return nil, nil
+		return nil, status.Errorf(codes.Internal, "failed to download media: %v", err)
 	}
 	return &__.DownloadMediaResponse{Content: resp}, nil
 }
